b4f60154d956bfcb8b99676b5d6f4c4429a606a8
Gabriella Koos <koos.gabriella92@gmail.com>
2024-06-04 09:35:05 +0200

    handle input with useState

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayer, setEnteredPlayer] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayer(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayer : "unknown"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={handleChange} value={enteredPlayer} />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
